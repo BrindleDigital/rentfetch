a16902ec1b9fb05d5e6b650ccc829eb9f685274b
Jon Schroeder <jon@elod.in>
2023-09-21 01:45:48 -0500

    Adding units admin screens

---
+++ lib/options-pages/admin-units-default-columns.php
@@ -0,0 +1,102 @@
+<?php
+
+add_action( 'admin_enqueue_scripts', 'rentfetch_enqueue_units_admin_style' );
+function rentfetch_enqueue_units_admin_style() {
+	
+	// bail if admin columns pro is active, or admin columns is active, since our styles conflict with those plugins
+	if ( is_plugin_active( 'admin-columns-pro/admin-columns-pro.php' ) || is_plugin_active( 'codepress-admin-columns/codepress-admin-columns.php' ) )
+		return;
+	
+	$current_screen = get_current_screen();
+  
+	// Check if the current screen is the admin archive page of the units content type
+	if ( $current_screen->base === 'edit' && $current_screen->post_type === 'units' ) {
+		
+		// Enqueue your custom admin style
+		wp_enqueue_style( 'units-edit-admin-style', RENTFETCH_PATH . 'css/admin/admin-edit-units.css', array(), RENTFETCH_VERSION, 'screen' );
+	}
+}
+
+add_filter( 'manage_units_posts_columns', 'rentfetch_default_units_admin_columns' );
+function rentfetch_default_units_admin_columns( $columns ) {
+	
+	$columns = array(
+		'cb' =>                        '<input type="checkbox" />',
+		'title' =>                     __( 'Title', 'rentfetch' ),
+		'unit_id' =>                   __( 'Unit ID', 'rentfetch' ),
+		'floorplan_id' =>              __( 'Floorplan ID', 'rentfetch' ),
+		'property_id' =>               __( 'Property ID', 'rentfetch' ),
+		'apply_online_url' =>          __( 'Apply URL', 'rentfetch' ),
+		'availability_date' =>          __( 'Availability date', 'rentfetch' ),
+		'baths' =>         				 __( 'Baths', 'rentfetch' ),
+		'beds' =>         				 __( 'Beds', 'rentfetch' ),
+		'deposit' =>       			   __( 'Deposit', 'rentfetch' ),
+		'minimum_rent' =>      		    __( 'Min Rent', 'rentfetch' ),
+		'maximum_rent' =>     	     __( 'Max Rent', 'rentfetch' ),
+		'sqrft' =>         			 __( 'Sqrft', 'rentfetch' ),
+		'specials' =>      				    __( 'Specials', 'rentfetch' ),
+		'yardi_unit_image_urls' =>          __( 'Yardi image', 'rentfetch' ),
+		'unit_source' =>          __( 'Integration', 'rentfetch' ),
+		'updated' =>          __( 'Updated', 'rentfetch' ),
+		'api_error' =>          __( 'API reponse', 'rentfetch' ),
+	);
+	
+	return $columns;
+	
+}
+
+add_action( 'manage_units_posts_custom_column', 'rentfetch_units_default_column_content', 10, 2);
+function rentfetch_units_default_column_content( $column, $post_id ) {
+		
+	if ( 'title' === $column )
+		echo esc_attr( get_the_title( $post_id ) );
+		
+	if ( 'unit_id' === $column )
+		echo esc_attr( get_post_meta( $post_id, 'unit_id', true ) );
+
+	if ( 'floorplan_id' === $column )
+		echo esc_attr( get_post_meta( $post_id, 'floorplan_id', true ) );
+
+	if ( 'property_id' === $column )
+		echo esc_attr( get_post_meta( $post_id, 'property_id', true ) );
+
+	if ( 'apply_online_url' === $column )
+		echo esc_attr( get_post_meta( $post_id, 'apply_online_url', true ) );
+
+	if ( 'availability_date' === $column )
+		echo esc_attr( get_post_meta( $post_id, 'availability_date', true ) );
+
+	if ( 'baths' === $column )
+		echo esc_attr( get_post_meta( $post_id, 'baths', true ) );
+
+	if ( 'beds' === $column )
+		echo esc_attr( get_post_meta( $post_id, 'beds', true ) );
+
+	if ( 'deposit' === $column )
+		echo esc_attr( get_post_meta( $post_id, 'deposit', true ) );
+
+	if ( 'minimum_rent' === $column )
+		echo esc_attr( get_post_meta( $post_id, 'minimum_rent', true ) );
+
+	if ( 'maximum_rent' === $column )
+		echo esc_attr( get_post_meta( $post_id, 'maximum_rent', true ) );
+
+	if ( 'sqrft' === $column )
+		echo esc_attr( get_post_meta( $post_id, 'sqrft', true ) );
+
+	if ( 'specials' === $column )
+		echo esc_attr( get_post_meta( $post_id, 'specials', true ) );
+
+	if ( 'yardi_unit_image_urls' === $column )
+		echo esc_attr( get_post_meta( $post_id, 'yardi_unit_image_urls', true ) );
+
+	if ( 'unit_source' === $column )
+		echo esc_attr( get_post_meta( $post_id, 'unit_source', true ) );
+
+	if ( 'updated' === $column )
+		echo esc_attr( get_post_meta( $post_id, 'updated', true ) );
+
+	if ( 'api_error' === $column )
+		echo esc_attr( get_post_meta( $post_id, 'api_error', true ) );
+	
+}
--- lib/template-functions/single-properties-parts/single-properties-floorplans.php
+++ lib/template-functions/single-properties-parts/single-properties-floorplans.php
@@ -61,7 +61,54 @@ function rentfetch_single_properties_parts_floorplans() {
 						echo '<div class="floorplan-in-archive">';
 							while ( $floorplans_query->have_posts() ) : $floorplans_query->the_post(); 
 							
+								echo '<div><strong>';
 								the_title();
+								echo '</strong></div>';
+								
+								$available_units = get_post_meta( get_the_ID(), 'available_units', true );
+								
+								printf( '%s Available', $available_units );
+								
+								$floorplan_id = get_post_meta( get_the_ID(), 'floorplan_id', true );
+								
+								$args = array(
+									'post_type' => 'units',
+									'posts_per_page' => -1,
+									'orderby' => 'meta_value_num',
+									'meta_key' => 'beds',
+									'order' => 'ASC',
+									'meta_query' => array(
+										array(
+											'key'   => 'property_id',
+											'value' => $property_id,
+										),
+										array(
+											'key'   => 'floorplan_id',
+											'value' => $floorplan_id,
+										),
+									),
+								);
+								
+								// The Query
+								$units_query = new WP_Query( $args );
+
+								// The Loop
+								if ( $units_query->have_posts() ) {
+
+									while ( $units_query->have_posts() ) {
+										
+										$units_query->the_post();
+										
+											echo '<div>';
+												the_title();
+											echo '</div>';
+
+									}
+									
+									// Restore postdata
+									wp_reset_postdata();
+
+								}
 							
 							endwhile;
 						echo '</div>'; // .floorplans
