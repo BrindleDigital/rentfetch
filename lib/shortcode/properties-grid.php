c9a5bda19148c259b5a99d0b22cdb7e9f2b97d75
Jon Schroeder <jon@elod.in>
2024-01-25 22:14:16 -0600

    Another tiny change

--- lib/shortcode/properties-grid.php
+++ lib/shortcode/properties-grid.php
@@ -1,51 +1,63 @@
 <?php
+/**
+ * This file sets up the rentfetch_properties shortcode.
+ *
+ * @package rentfetch
+ */
 
-if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
 
 /**
- * The simple properties template
+ * This function sets up the rentfetch_properties shortcode.
  *
+ * @param  array $atts  the shortcode attributes.
+ * @return string       the shortcode output.
  */
 function rentfetch_properties( $atts ) {
-	$a = shortcode_atts( array(
-		'foo' => 'something',
-		'bar' => 'something else',
-	), $atts );
-
-	ob_start();
-	
-	$args = array(
-		'post_type' => 'properties',
-		'posts_per_page' => '-1'
+	$a = shortcode_atts(
+		array(
+			'foo' => 'something',
+			'bar' => 'something else',
+		),
+		$atts
 	);
 
-	// The Query
+	ob_start();
+
+	$args = array(
+		'post_type'      => 'properties',
+		'posts_per_page' => '-1',
+	);
+
+	// Run the query.
 	$custom_query = new WP_Query( $args );
 
-	// The Loop
+	// Run the loop.
 	if ( $custom_query->have_posts() ) {
-		
+
 		echo '<div class="properties-simple-grid">';
 
 		while ( $custom_query->have_posts() ) {
-			
+
 			$custom_query->the_post();
 
-			printf( '<div class="%s">', implode( ' ', get_post_class() ) );
-			
+			printf( '<div class="%s">', esc_attr( implode( ' ', get_post_class() ) ) );
+
 				do_action( 'rentfetch_do_each_property_in_archive' );
 
 			echo '</div>';
 
 		}
-		
+
 		echo '</div>';
-		
-		// Restore postdata
+
+		// Restore postdata.
 		wp_reset_postdata();
 
 	}
-	
+
 	return ob_get_clean();
 }
 add_shortcode( 'properties', 'rentfetch_properties' );
