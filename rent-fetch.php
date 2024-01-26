c6af6919c8141f8733cb73dc94d69efc2656fd28
Jon Schroeder <jon@elod.in>
2024-01-25 21:36:10 -0600

    wordpress codesniffer used on the core file

--- rent-fetch.php
+++ rent-fetch.php
@@ -1,32 +1,32 @@
 <?php
 /*
-	Plugin Name:	Rent Fetch
-	Plugin URI:		http://wordpress.org/plugins/rent-fetch/
-	Description:	Displays searchable rental properties, floorplans, and unit availability.
-	Version:		0.12.6
-	Author:			Brindle Digital
-	Author URI:		https://www.brindledigital.com
-	Text Domain:	rentfetch
-	License:		GPLv2 or later
-	License URI:	http://www.gnu.org/licenses/gpl-2.0.html
+	Plugin Name:    Rent Fetch
+	Plugin URI:     http://wordpress.org/plugins/rent-fetch/
+	Description:    Displays searchable rental properties, floorplans, and unit availability.
+	Version:        0.12.6
+	Author:         Brindle Digital
+	Author URI:     https://www.brindledigital.com
+	Text Domain:    rentfetch
+	License:        GPLv2 or later
+	License URI:    http://www.gnu.org/licenses/gpl-2.0.html
 */
 
 /* Prevent direct access to the plugin */
-if ( !defined( 'ABSPATH' ) ) {
-	die( "Sorry, you are not allowed to access this page directly." );
+if ( ! defined( 'ABSPATH' ) ) {
+	die( 'Sorry, you are not allowed to access this page directly.' );
 }
 
 // Define the version of the plugin
-define ( 'RENTFETCH_VERSION', '0.12.6' );
+define( 'RENTFETCH_VERSION', '0.12.6' );
 
-// Plugin directories 
+// Plugin directories
 define( 'RENTFETCH_DIR', plugin_dir_path( __FILE__ ) );
 define( 'RENTFETCH_PATH', plugin_dir_url( __FILE__ ) );
 define( 'RENTFETCH_BASENAME', plugin_basename( __FILE__ ) );
 
-///////////////////
+//
 // FILE INCLUDES //
-///////////////////
+//
 
 function rentfetch_require_files_recursive( $directory ) {
 	$iterator = new RecursiveIteratorIterator(
@@ -34,8 +34,8 @@ function rentfetch_require_files_recursive( $directory ) {
 		RecursiveIteratorIterator::LEAVES_ONLY
 	);
 
-	foreach ($iterator as $file) {
-		if ($file->isFile() && $file->getExtension() === 'php') {
+	foreach ( $iterator as $file ) {
+		if ( $file->isFile() && $file->getExtension() === 'php' ) {
 			require_once $file->getPathname();
 		}
 	}
@@ -45,14 +45,14 @@ function rentfetch_require_files_recursive( $directory ) {
 rentfetch_require_files_recursive( RENTFETCH_DIR . 'lib' );
 
 // Flush the permalinks after plugin is activated
-function rentfetch_flush_permalinks_on_activation() {	
-	add_action('init', 'flush_rewrite_rules', 999 );
+function rentfetch_flush_permalinks_on_activation() {
+	add_action( 'init', 'flush_rewrite_rules', 999 );
 }
 register_activation_hook( RENTFETCH_BASENAME, 'rentfetch_flush_permalinks_on_activation' );
 
-////////////////////
+//
 // PLUGIN UPDATER //
-////////////////////
+//
 
 // Updater
 require RENTFETCH_DIR . 'vendor/plugin-update-checker/plugin-update-checker.php';
