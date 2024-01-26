88e697912dc7d26ed4d0d7cf2b90e6c19e9bff0a
Jon Schroeder <jon@elod.in>
2024-01-25 21:48:57 -0600

    Various codestandard fixes

--- rent-fetch.php
+++ rent-fetch.php
@@ -1,5 +1,5 @@
 <?php
-/*
+/**
 	Plugin Name:    Rent Fetch
 	Plugin URI:     http://wordpress.org/plugins/rent-fetch/
 	Description:    Displays searchable rental properties, floorplans, and unit availability.
@@ -9,23 +9,29 @@
 	Text Domain:    rentfetch
 	License:        GPLv2 or later
 	License URI:    http://www.gnu.org/licenses/gpl-2.0.html
-*/
+ *
+	@package rentfetch
+ */
 
-/* Prevent direct access to the plugin */
+// Prevent direct access to the plugin.
 if ( ! defined( 'ABSPATH' ) ) {
 	die( 'Sorry, you are not allowed to access this page directly.' );
 }
 
-// Define the version of the plugin
+// Define the version of the plugin.
 define( 'RENTFETCH_VERSION', '0.12.6' );
 
-// Plugin directories
+// Set up plugin directories.
 define( 'RENTFETCH_DIR', plugin_dir_path( __FILE__ ) );
 define( 'RENTFETCH_PATH', plugin_dir_url( __FILE__ ) );
 define( 'RENTFETCH_BASENAME', plugin_basename( __FILE__ ) );
 
-//* FILE INCLUDES //
-
+/**
+ * Load the files
+ *
+ * @param   string $directory  the path to the directory to load.
+ * @return  void
+ */
 function rentfetch_require_files_recursive( $directory ) {
 	$iterator = new RecursiveIteratorIterator(
 		new RecursiveDirectoryIterator( $directory, RecursiveDirectoryIterator::SKIP_DOTS ),
@@ -39,24 +45,26 @@ function rentfetch_require_files_recursive( $directory ) {
 	}
 }
 
-// require_once all files in /lib and its subdirectories
+// Require_once all files in /lib and its subdirectories.
 rentfetch_require_files_recursive( RENTFETCH_DIR . 'lib' );
 
-// Flush the permalinks after plugin is activated
+/**
+ * Flush the permalinks after plugin is activated.
+ *
+ * @return  void
+ */
 function rentfetch_flush_permalinks_on_activation() {
 	add_action( 'init', 'flush_rewrite_rules', 999 );
 }
 register_activation_hook( RENTFETCH_BASENAME, 'rentfetch_flush_permalinks_on_activation' );
 
-//* PLUGIN UPDATER //
-
-// Updater
+// Load Plugin Update Checker.
 require RENTFETCH_DIR . 'vendor/plugin-update-checker/plugin-update-checker.php';
-$myUpdateChecker = Puc_v4_Factory::buildUpdateChecker(
+$update_checker = Puc_v4_Factory::buildUpdateChecker(
 	'https://github.com/BrindleDigital/rentfetch',
 	__FILE__,
 	'rentfetch'
 );
 
 // Optional: Set the branch that contains the stable release.
-$myUpdateChecker->setBranch( 'main' );
+$update_checker->setBranch( 'main' );
