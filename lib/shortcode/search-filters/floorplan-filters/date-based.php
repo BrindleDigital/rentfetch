5555e7255fc5a5a9f32c6f09f169bbd17f67347c
Jon Schroeder <jon@elod.in>
2024-02-07 22:37:44 -0600

    Code standards

--- lib/shortcode/search-filters/floorplan-filters/date-based.php
+++ lib/shortcode/search-filters/floorplan-filters/date-based.php
@@ -15,59 +15,56 @@ if ( ! defined( 'ABSPATH' ) ) {
  * @return void.
  */
 function rentfetch_search_filters_date() {
-			
-	// enqueue date picker scripts
-	// wp_enqueue_style( 'rentfetch-flatpickr-style' );
-	// wp_enqueue_script( 'rentfetch-flatpickr-script' );
-	// wp_enqueue_script( 'rentfetch-flatpickr-script-init' );
-	
+
 	$date = null;
-	
-	if ( isset( $_GET['dates'] ) && $_GET['dates'] > 0 )
-		$date = sanitize_text_field( $_GET['dates'] );
-	
-	// build the date-based search
+
+	if ( isset( $_GET['dates'] ) && $_GET['dates'] > 0 ) {
+		$date = sanitize_text_field( wp_unslash( $_GET['dates'] ) );
+	}
+
+	// build the date-based search.
 	echo '<fieldset class="move-in">';
 		echo '<legend>Move-In Date</legend>';
 		echo '<button class="toggle">Move-In Date</button>';
 		echo '<div class="input-wrap inactive">';
-			printf( '<input type="date" value="%s" name="dates" placeholder="Available date" style="width:auto;" data-input />', $date );
-		echo '</div>'; // .input-wrap
+			printf( '<input type="date" value="%s" name="dates" placeholder="Available date" style="width:auto;" data-input />', esc_html( $date ) );
+		echo '</div>'; // .input-wrap.
 	echo '</fieldset>';
 }
 
+/**
+ * Add the date-based filter to the search filters
+ *
+ * @param array $floorplans_args The floorplan arguments.
+ *
+ * @return array.
+ */
 function rentfetch_search_floorplans_args_date( $floorplans_args ) {
-	
-	// TODO: this filter currently controls whether we do an availability search or not; if possible this should maybe move to a separate filter
-		
-	if ( !isset( $_POST['dates'] ) )
+
+	if ( ! isset( $_POST['dates'] ) ) {
 		return $floorplans_args;
-	
-	if ( empty( $_POST['dates'] ) )
+	}
+
+	if ( empty( $_POST['dates'] ) ) {
 		return $floorplans_args;
-	
-	$date = $_POST['dates'];
-	
-	// get the dates, in a format like this: 'YYYYMMDD to YYYYMMDD'
-	$yesterday = date('Ymd',strtotime("-30 days"));
-	$datestring = date('Ymd', strtotime(sanitize_text_field($date)));
-					
-	// do a between query between yesterday and the date entered
+	}
+
+	$date = sanitize_text_field( wp_unslash( $_POST['dates'] ) );
+
+	// get the dates, in a format like this: 'YYYYMMDD to YYYYMMDD'.
+	$yesterday  = gmdate( 'Ymd', strtotime( '-30 days' ) );
+	$datestring = gmdate( 'Ymd', strtotime( sanitize_text_field( $date ) ) );
+
+	// do a between query between yesterday and the date entered.
 	$floorplans_args['meta_query'][] = array(
-		'relation' => 'OR', // we need to make sure that the date is between yesterday and the selected date OR that there are already units available
 		array(
-			'key' => 'availability_date',
-			'value' => array( $yesterday, $datestring ),
-			'type' => 'numeric',
+			'key'     => 'availability_date',
+			'value'   => array( $yesterday, $datestring ),
+			'type'    => 'numeric',
 			'compare' => 'BETWEEN',
 		),
-		array(
-			'key' => 'available_units',
-			'compare' => '>',
-			'value' => 0,
-		),
-	);		
-	
+	);
+
 	return $floorplans_args;
 }
 add_filter( 'rentfetch_search_floorplans_query_args', 'rentfetch_search_floorplans_args_date', 10, 1 );
