c90b86517420092da3e5a5e3d9b540c21531cd9e
Jon Schroeder <jon@elod.in>
2024-02-09 13:45:03 -0600

    Code standards

--- template/single-floorplans.php
+++ template/single-floorplans.php
@@ -12,13 +12,13 @@ if ( ! defined( 'ABSPATH' ) ) {
 get_header();
 
 if ( have_posts() ) {
-	
+
 	while ( have_posts() ) {
-				
-		the_post(); 
-		
+
+		the_post();
+
 		global $post;
-		
+
 		echo '<div class="single-floorplans-container-outer">';
 			echo '<div class="single-floorplans-container-inner">';
 				echo '<div class="current-floorplan-info">';
@@ -27,66 +27,69 @@ if ( have_posts() ) {
 						do_action( 'rentfetch_do_floorplan_images' );
 					echo '</div>';
 					echo '<div class="content-column">';
-									
-						// vars
-						$title = rentfetch_get_floorplan_title();
-						$beds = rentfetch_get_floorplan_bedrooms();
-						$baths = rentfetch_get_floorplan_bathrooms();
-						$square_feet = rentfetch_get_floorplan_square_feet();
+
+						// vars.
+						$floorplan_title = rentfetch_get_floorplan_title();
+						$beds            = rentfetch_get_floorplan_bedrooms();
+						$baths           = rentfetch_get_floorplan_bathrooms();
+						$square_feet     = rentfetch_get_floorplan_square_feet();
 						$available_units = rentfetch_get_floorplan_available_units();
-						$links = rentfetch_get_floorplan_links();
-						$pricing = rentfetch_get_floorplan_pricing();     
-						$units_count = rentfetch_get_floorplan_units_count_from_meta(); 
-						
-						
-						if ( $title )
-							printf( '<h1>%s</h1>', $title );
-						
-						if ( $pricing )
-							printf( '<p class="pricing">%s</p>', $pricing );
-						
+						$links           = rentfetch_get_floorplan_links();
+						$pricing         = rentfetch_get_floorplan_pricing();
+						$units_count     = rentfetch_get_floorplan_units_count_from_meta();
+
+						if ( $floorplan_title ) {
+							printf( '<h1>%s</h1>', esc_html( $floorplan_title ) );
+						}
+
+						if ( $pricing ) {
+							printf( '<p class="pricing">%s</p>', wp_kses_post( $pricing ) );
+						}
+
 						echo '<div class="floorplan-attributes">';
-						
-							if ( $beds )
-								printf( '<p class="beds">%s</p>', $beds );
-							
-							if ( $baths )
-								printf( '<p class="baths">%s</p>', $baths );
-										
-							if ( $square_feet )
-								printf( '<p class="square-feet">%s</p>', $square_feet );
-						
+
+							if ( $beds ) {
+								printf( '<p class="beds">%s</p>', wp_kses_post( $beds ) );
+							}
+
+							if ( $baths ) {
+								printf( '<p class="baths">%s</p>', wp_kses_post( $baths ) );
+							}
+
+							if ( $square_feet ) {
+								printf( '<p class="square-feet">%s</p>', wp_kses_post( $square_feet ) );
+							}
+
 						echo '</div>';
-						
+
 						if ( $units_count > 0 ) {
 							// printf( '<p class="availability">%s</p>', $available_units );
-							
-							// typically there will be two things hooked to this, a desktop <table> and a mobile <details>
+
+							// typically there will be two things hooked to this, a desktop <table> and a mobile <details>.
 							do_action( 'rentfetch_floorplan_do_unit_table' );
 						}
-						
-						echo '</div>'; // .content-column
+
+					echo '</div>'; // .content-column
 				echo '</div>'; // .current-floorplan-info
 			echo '</div>'; // .container-inner
 		echo '</div>'; // .container-outer
 		// echo '<div class="single-floorplans-container-outer">';
-		// 	echo '<div class="single-floorplans-container-inner">';
-		// 		echo '<h2>Take a look around</h2>';
-		// 	echo '</div>'; // .container-inner
+		// echo '<div class="single-floorplans-container-inner">';
+		// echo '<h2>Take a look around</h2>';
+		// echo '</div>'; // .container-inner
 		// echo '</div>'; // .container-outer
 		// echo '<div class="single-floorplans-container-outer">';
-		// 	echo '<div class="single-floorplans-container-inner">';
-		// 		echo '<h2>Similar floorplans</h2>';
-		// 	echo '</div>'; // .container-inner
+		// echo '<div class="single-floorplans-container-inner">';
+		// echo '<h2>Similar floorplans</h2>';
+		// echo '</div>'; // .container-inner
 		// echo '</div>'; // .container-outer
-		
-		
+
+
 	} // end while
-	
 } else {
 	echo 'So sorry! Nothing found.';
 }
-	
-		
+
+
 
 get_footer();
--- template/single-properties.php
+++ template/single-properties.php
@@ -11,11 +11,11 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 get_header();
 
-//* Markup
+// * Markup.
 echo '<div class="single-properties-wrap">';
-	
+
 	do_action( 'rentfetch_do_single_properties_parts' );
-	
-echo '</div>'; // .single-properties-wrap
+
+echo '</div>'; // .single-properties-wrap.
 
 get_footer();
