c420764e7736396daecf230560b9bf3e4b8319a0
Jon Schroeder <jon@elod.in>
2024-02-09 13:40:15 -0600

    Code standards

--- lib/template-functions/single-properties-parts/single-properties-parts-setup.php
+++ lib/template-functions/single-properties-parts/single-properties-parts-setup.php
@@ -9,8 +9,13 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly.
 }
 
+/**
+ * Add the sections to the single property page (for easy re-ordering these are on a hook)
+ *
+ * @return void.
+ */
 function rentfetch_single_properties_set_up_parts() {
-	
+
 	add_action( 'rentfetch_do_single_properties_parts', 'rentfetch_single_properties_parts_images' );
 	add_action( 'rentfetch_do_single_properties_parts', 'rentfetch_single_properties_parts_subnav' );
 	add_action( 'rentfetch_do_single_properties_parts', 'rentfetch_single_properties_parts_details' );
@@ -18,18 +23,21 @@ function rentfetch_single_properties_set_up_parts() {
 	add_action( 'rentfetch_do_single_properties_parts', 'rentfetch_single_properties_parts_amenities' );
 	add_action( 'rentfetch_do_single_properties_parts', 'rentfetch_single_properties_parts_map' );
 	add_action( 'rentfetch_do_single_properties_parts', 'rentfetch_single_properties_parts_more_properties' );
-	
 }
 add_action( 'wp_loaded', 'rentfetch_single_properties_set_up_parts' );
 
+/**
+ * Set up the navigation components (for easy re-ordering these are on a hook)
+ *
+ * @return void.
+ */
 function rentfetch_single_properties_set_up_subnav_parts() {
-	
+
 	add_action( 'rentfetch_do_single_properties_subnav_parts', 'rentfetch_single_properties_parts_subnav_images' );
 	add_action( 'rentfetch_do_single_properties_subnav_parts', 'rentfetch_single_properties_parts_subnav_details' );
-	add_action( 'rentfetch_do_single_properties_subnav_parts', 'rentfetch_single_properties_parts_subnav_floorplans' );		
+	add_action( 'rentfetch_do_single_properties_subnav_parts', 'rentfetch_single_properties_parts_subnav_floorplans' );
 	add_action( 'rentfetch_do_single_properties_subnav_parts', 'rentfetch_single_properties_parts_subnav_amenities' );
 	add_action( 'rentfetch_do_single_properties_subnav_parts', 'rentfetch_single_properties_parts_subnav_maps' );
 	add_action( 'rentfetch_do_single_properties_subnav_parts', 'rentfetch_single_properties_parts_subnav_more_properties' );
-	
 }
 add_action( 'wp_loaded', 'rentfetch_single_properties_set_up_subnav_parts' );
--- lib/template-functions/single-properties-parts/single-properties-subnav.php
+++ lib/template-functions/single-properties-parts/single-properties-subnav.php
@@ -9,25 +9,30 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly.
 }
 
+/**
+ * Output the wrapper for the subnav
+ *
+ * @return void.
+ */
 function rentfetch_single_properties_parts_subnav() {
-	
-	$maybe_do_subnav = apply_filters( 'rentfetch_maybe_do_property_part_subnav', true );    
-	if ( $maybe_do_subnav !== true )
+
+	$maybe_do_subnav = apply_filters( 'rentfetch_maybe_do_property_part_subnav', true );
+	if ( true !== $maybe_do_subnav ) {
 		return;
-	
+	}
+
 	wp_enqueue_script( 'properties-single-collapse-subnav' );
-	
+
 	echo '<div id="subnav" class="single-properties-section no-padding">';
 		echo '<div class="wrap">';
-		
+
 			echo '<a class="toggle-subnav" href="#">Quick links <span class="dashicons dashicons-arrow-down-alt2"></span></a>';
-		
-			echo '<ul class="subnav">';				
-				
+
+			echo '<ul class="subnav">';
+
 				do_action( 'rentfetch_do_single_properties_subnav_parts' );
-			
-			echo '</ul>'; // .subnav
-		echo '</div>'; // .wrap
-	echo '</div>'; // #subnav
-	
+
+			echo '</ul>'; // .subnav.
+		echo '</div>'; // .wrap.
+	echo '</div>'; // #subnav.
 }
