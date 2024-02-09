8e065d7469d4feb1959f60183c27cc7517ee7f24
Jon Schroeder <jon@elod.in>
2024-02-09 13:24:54 -0600

    Code standards

--- lib/template-functions/single-properties-parts/single-properties-floorplans.php
+++ lib/template-functions/single-properties-parts/single-properties-floorplans.php
@@ -12,131 +12,136 @@ if ( ! defined( 'ABSPATH' ) ) {
 /**
  * Output the floorplans section
  *
+ * @return void.
  */
 function rentfetch_single_properties_parts_floorplans() {
-	
-	$maybe_do_floorplans = apply_filters( 'rentfetch_maybe_do_property_part_floorplans', true );    
-	if ( $maybe_do_floorplans !== true )
+
+	$maybe_do_floorplans = apply_filters( 'rentfetch_maybe_do_property_part_floorplans', true );
+	if ( true !== $maybe_do_floorplans ) {
 		return;
-	
+	}
+
 	echo '<div id="floorplans" class="single-properties-section">';
 		echo '<div class="wrap">';
-		
-		echo '<h2>Floorplans</h2>';
-		
-		global $post;
-		
-		$id = esc_attr( get_the_ID() );
-		$property_id = esc_attr( get_post_meta( $id, 'property_id', true ) );
-		
-		// grab the gravity forms lightbox, if enabled on this page
-		// do_action( 'rentfetch_do_gform_lightbox' );
-		
-		// get the possible values for the beds
-		$beds = rentfetch_get_meta_values( 'beds', 'floorplans' );
-		$beds = array_unique( $beds );
-		asort( $beds );
-		
-			// loop through each of the possible values, so that we can do markup around that
-			foreach( $beds as $bed ) {
-				
+
+			echo '<h2>Floorplans</h2>';
+
+			global $post;
+
+			$id          = esc_attr( get_the_ID() );
+			$property_id = esc_attr( get_post_meta( $id, 'property_id', true ) );
+
+			// get the possible values for the beds.
+			$beds = rentfetch_get_meta_values( 'beds', 'floorplans' );
+			$beds = array_unique( $beds );
+			asort( $beds );
+
+			// loop through each of the possible values, so that we can do markup around that.
+			foreach ( $beds as $bed ) {
+
 				$args = array(
-					'post_type' => 'floorplans',
+					'post_type'      => 'floorplans',
 					'posts_per_page' => -1,
-					'orderby' => 'meta_value_num',
-					'meta_key' => 'beds',
-					'order' => 'ASC',
-					'meta_query' => array(
+					'orderby'        => 'meta_value_num',
+					'meta_key'       => 'beds',
+					'order'          => 'ASC',
+					'meta_query'     => array(
 						array(
 							'key'   => 'property_id',
 							'value' => $property_id,
 						),
 						array(
-							'key' => 'beds',
+							'key'   => 'beds',
 							'value' => $bed,
 						),
 					),
 				);
-				
+
 				$floorplans_query = new WP_Query( $args );
-					
+
 				if ( $floorplans_query->have_posts() ) {
-					// echo '<details open>';
-						// echo '<summary><h3>';
-						
-						echo '<div class="floorplan-group">';
-												
-							echo '<h3>';                 
-								echo apply_filters( 'rentfetch_get_bedroom_number_label', $bed );
-							echo '</h3>';
-							// echo '</h3></summary>';
-							echo '<div class="floorplans-in-archive">';
-							
-								while ( $floorplans_query->have_posts() ) : $floorplans_query->the_post(); 
-								
-									$class = implode( ' ', get_post_class() );
-							
-									printf( '<div class="%s">', $class );
-									
-										do_action( 'rentfetch_single_properties_do_floorplans_each' );
-									
-									echo '</div>'; // post_class
-									
-								endwhile;
-							echo '</div>'; // .floorplans
-						
-						echo '</div>';
-					// echo '</details>';
-					
+
+					echo '<div class="floorplan-group">';
+
+						echo '<h3>';
+							echo wp_kses_post( apply_filters( 'rentfetch_get_bedroom_number_label', $bed ) );
+						echo '</h3>';
+						echo '<div class="floorplans-in-archive">';
+
+							while ( $floorplans_query->have_posts() ) {
+
+								$floorplans_query->the_post();
+
+								$class = implode( ' ', get_post_class() );
+
+								printf( '<div class="%s">', esc_attr( $class ) );
+
+									do_action( 'rentfetch_single_properties_do_floorplans_each' );
+
+								echo '</div>'; // post_class.
+
+							}
+
+						echo '</div>'; // .floorplans-in-archive.
+
+					echo '</div>'; // .floorplan-group.
+
+					wp_reset_postdata();
 				}
-				
-				wp_reset_postdata();
 			}
-		
-		
-		echo '</div>'; // .wrap
-	echo '</div>'; // #floorplans
-	
+
+		echo '</div>'; // .wrap.
+	echo '</div>'; // #floorplans.
 }
 
 /**
  * Decide whether to output the floorplans section
  */
 function rentfetch_maybe_property_part_floorplans() {
-	
-	// bail if this section is not enabled
+
+	// bail if this section is not enabled.
 	$property_components = get_option( 'rentfetch_options_single_property_components' );
-	if ( !is_array( $property_components ) || !in_array( 'floorplans_display', $property_components ) )
+	if ( ! is_array( $property_components ) || ! in_array( 'floorplans_display', $property_components, true ) ) {
 		return false;
-		
-	// bail if this property doesn't have any floorplans
-	$floorplans = get_posts( array(
-		'post_type' => 'floorplans',
-		'posts_per_page' => -1,
-		'meta_query' => array(
-			array(
-				'key' => 'property_id',
-				'value' => get_post_meta( get_the_ID(), 'property_id', true ),
-				'compare' => '='
-			)
+	}
+
+	// bail if this property doesn't have any floorplans.
+	$floorplans = get_posts(
+		array(
+			'post_type'      => 'floorplans',
+			'posts_per_page' => -1,
+			'meta_query'     => array(
+				array(
+					'key'     => 'property_id',
+					'value'   => get_post_meta( get_the_ID(), 'property_id', true ),
+					'compare' => '=',
+				),
+			),
 		)
-	) );
-	
-	if ( !$floorplans )
+	);
+
+	if ( ! $floorplans ) {
 		return false;
-		
-	// bail if this property doesn't have an ID
-	if ( !get_post_meta( get_the_ID(), 'property_id', true ) )
+	}
+
+	// bail if this property doesn't have an ID.
+	if ( ! get_post_meta( get_the_ID(), 'property_id', true ) ) {
 		return false;
-		
+	}
+
 	return true;
 }
 add_filter( 'rentfetch_maybe_do_property_part_floorplans', 'rentfetch_maybe_property_part_floorplans' );
 
+/**
+ * Output the floorplans section in the subnav if it should be displayed 
+ *
+ * @return void.
+ */
 function rentfetch_single_properties_parts_subnav_floorplans() {
 	$maybe_do_floorplans = apply_filters( 'rentfetch_maybe_do_property_part_floorplans', true );
-	if ( $maybe_do_floorplans === true ) {
+	if ( true === $maybe_do_floorplans ) {
 		$label = apply_filters( 'rentfetch_floorplans_display_subnav_label', 'Floorplans' );
-		printf( '<li><a href="#floorplans">%s</a></li>', $label );
+		printf( '<li><a href="#floorplans">%s</a></li>', esc_attr( $label ) );
 	}
 }
--- lib/template-functions/single-properties-parts/single-properties-google-maps.php
+++ lib/template-functions/single-properties-parts/single-properties-google-maps.php
@@ -10,75 +10,85 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 /**
- * Output the google maps section
+ * Output the Google Maps section
  *
+ * @return void.
  */
 function rentfetch_single_properties_parts_map() {
-	
-	$maybe_do_map = apply_filters( 'rentfetch_maybe_do_property_part_maps', true );    
-	if ( $maybe_do_map !== true )
+
+	$maybe_do_map = apply_filters( 'rentfetch_maybe_do_property_part_maps', true );
+	if ( true !== $maybe_do_map ) {
 		return;
-	
+	}
+
 	$id = esc_attr( get_the_ID() );
 
-	$latitude = floatval( get_post_meta( $id, 'latitude', true ) );
+	$latitude  = floatval( get_post_meta( $id, 'latitude', true ) );
 	$longitude = floatval( get_post_meta( $id, 'longitude', true ) );
-	
-	//* bail if there's not a lat or longitude
-	if ( empty( $latitude ) || empty( $longitude) )
+
+	// * bail if there's not a lat or longitude
+	if ( empty( $latitude ) || empty( $longitude ) ) {
 		return;
-	
+	}
+
 	echo '<div id="googlemaps" class="single-properties-section no-padding full-width">';
 		echo '<div class="wrap">';
-		
-			$title = esc_attr( rentfetch_get_property_title() );
-			$phone = esc_attr( rentfetch_get_property_phone() );
+
+			$title    = esc_attr( rentfetch_get_property_title() );
+			$phone    = esc_attr( rentfetch_get_property_phone() );
 			$location = esc_attr( rentfetch_get_property_location() );
-			
+
 			$content = sprintf( '<div class="map-marker"><p class="title">%s</p><p class="location">%s</p></div>', $title, $location );
 			$content = esc_attr( apply_filters( 'rentfetch_property_single_map_marker_content', $content ) );
-			
-			// the map itself
-			$key = apply_filters( 'rentfetch_get_google_maps_api_key', null );
-			wp_enqueue_script( 'rentfetch-google-maps', 'https://maps.googleapis.com/maps/api/js?key=' . $key, array(), null, true );
 
-			// Localize the google maps script, then enqueue that
+			// the map itself.
+			$key = apply_filters( 'rentfetch_get_google_maps_api_key', null );
+			wp_enqueue_script( 'rentfetch-google-maps', 'https://maps.googleapis.com/maps/api/js?key=' . $key, array(), RENTFETCH_VERSION, true );
+
+			// Localize the google maps script, then enqueue that.
 			$maps_options = array(
 				'json_style' => json_decode( get_option( 'rentfetch_options_google_maps_styles' ) ),
 				'marker_url' => get_option( 'rentfetch_options_google_map_marker' ),
-				'latitude' => $latitude,
-				'longitude' => $longitude,
-				'content' => $content,
+				'latitude'   => $latitude,
+				'longitude'  => $longitude,
+				'content'    => $content,
 			);
 			wp_localize_script( 'rentfetch-single-property-map', 'options', $maps_options );
-			wp_enqueue_script( 'rentfetch-single-property-map');
-			
+			wp_enqueue_script( 'rentfetch-single-property-map' );
+
 			echo '<div id="single-property-map"></div>';
-		
-		echo '</div>'; // .wrap
-	echo '</div>'; // #googlemaps
-	
+
+			echo '</div>'; // .wrap
+			echo '</div>'; // #googlemaps
 }
 
 /**
- * Decide whether to output the maps section
+ * Determine if the Google Maps section should be displayed
+ *
+ * @return bool.
  */
 function rentfetch_maybe_property_part_maps() {
-	
-	// bail if this section is not enabled
+
+	// bail if this section is not enabled.
 	$property_components = get_option( 'rentfetch_options_single_property_components' );
-		
-	if ( !is_array( $property_components ) || !in_array( 'property_map', $property_components ) )
+
+	if ( ! is_array( $property_components ) || ! in_array( 'property_map', $property_components, true ) ) {
 		return false;
-		
+	}
+
 	return true;
 }
 add_filter( 'rentfetch_maybe_do_property_part_maps', 'rentfetch_maybe_property_part_maps' );
 
+/**
+ * Output the Google Maps section in the subnav
+ *
+ * @return void.
+ */
 function rentfetch_single_properties_parts_subnav_maps() {
 	$maybe_do_map = apply_filters( 'rentfetch_maybe_do_property_part_maps', true );
-	if ( $maybe_do_map === true ) {
+	if ( true === $maybe_do_map ) {
 		$label = apply_filters( 'rentfetch_property_map_subnav_label', 'Map' );
-		printf( '<li><a href="#googlemaps">%s</a></li>', $label );
+		printf( '<li><a href="#googlemaps">%s</a></li>', esc_attr( $label ) );
 	}
 }
