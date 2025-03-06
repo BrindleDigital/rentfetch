19d913c7286618fa36711baeceb6299310c308dd
elodin <jon@elod.in>
2025-03-06 13:42:38 -0600

    Updating a few global colors to make their variables more generic

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,17 +1,16 @@
 const mix = require('laravel-mix');
 const globImporter = require('node-sass-glob-importer');
 
-mix.setPublicPath('./')
-	.sass('css/rentfetch-style.scss', 'css', {
-		sassOptions: {
-			importer: globImporter(),
-		},
-	})
+mix.sass('css/rentfetch-style.scss', 'css', {
+	sassOptions: {
+		importer: globImporter(),
+	},
+})
 	.sass('css/admin.scss', 'css', {
 		sassOptions: {
 			importer: globImporter(),
 		},
 	})
 	.options({
-		processCssUrls: false, // Disable URL processing
+		processCssUrls: false,
 	});
