1c4b39aafc078f71ae64bf86fbe9f8a5cc7742b2
elodin <jon@elod.in>
2025-11-19 11:02:12 -0600

    adding an icon for the video tour button

--- lib/common/functions-properties.php
+++ lib/common/functions-properties.php
@@ -861,7 +861,10 @@ function rentfetch_get_property_tour_button( $property_id = null, $class = '' ) 
 	$iframe    = get_post_meta( $post_id, 'tour', true );
 	$embedlink = null;
 	$tour_link_text = 'Video Tour';
-	$svg = '<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6 tour-icon"><path stroke-linecap="round" stroke-linejoin="round" d="M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" /><path stroke-linecap="round" stroke-linejoin="round" d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1 1 15 0Z" /></svg>';
+	$svg = '<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6 tour-icon">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+  <path stroke-linecap="round" stroke-linejoin="round" d="M15.91 11.672a.375.375 0 0 1 0 .656l-5.603 3.113a.375.375 0 0 1-.557-.328V8.887c0-.286.307-.466.557-.327l5.603 3.112Z" />
+</svg>';
 	
 	// bail if we don't have anything to show.
 	if ( ! $iframe ) {
