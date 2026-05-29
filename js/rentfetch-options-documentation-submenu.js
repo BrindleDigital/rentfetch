4c3d4cffc6e4df88b41ca57b4a31d01f1cebd1d2
elodin <jon@elod.in>
2026-05-28 23:36:19 -0500

    Update the documentation link.

--- js/rentfetch-options-documentation-submenu.js
+++ js/rentfetch-options-documentation-submenu.js
@@ -2,7 +2,7 @@ jQuery(document).ready(function ($) {
 	$('a[href="admin.php?page=rentfetch-documentation"').each(function () {
 		if ($(this).text() == 'Documentation') {
 			$(this).css('color', 'yellow');
-			$(this).attr('href', 'https://github.com/BrindleDigital/rentfetch');
+			$(this).attr('href', 'https://rentfetch.io/docs/getting-started/');
 			$(this).attr('target', '_blank');
 		}
 	});
